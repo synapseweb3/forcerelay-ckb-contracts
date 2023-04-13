d3ea2f062f02079374643c7c9af5db270da895af
ImJeremyHe <yiliang.he@qq.com>
2023-04-13 13:39:41 +0800

    feat: add ics contracts

---
+++ contracts/ics/conn/src/error.rs
@@ -0,0 +1,42 @@
+use core::result;
+
+use ckb_std::error::SysError;
+
+pub type Result<T> = result::Result<T, Error>;
+
+#[repr(i8)]
+pub enum Error {
+    IndexOutOfBound = 1,
+    ItemMissing,
+    LengthNotEnough,
+    Encoding,
+    UnknownSysError,
+
+    ClientEncoding,
+    ConnectionEncoding,
+    EnvelopeEncoding,
+    MsgEncoding,
+
+    WitnessIsIncorrect,
+    WitnessTooMany,
+
+    LoadScriptErr,
+    LoadCellDataErr,
+
+    UnexpectedMsg,
+    ConnectionProofInvalid,
+    ConnectionHashUnmatch,
+}
+
+impl From<SysError> for Error {
+    fn from(err: SysError) -> Self {
+        use SysError::*;
+        match err {
+            IndexOutOfBound => Self::IndexOutOfBound,
+            ItemMissing => Self::ItemMissing,
+            LengthNotEnough(_) => Self::LengthNotEnough,
+            Encoding => Self::Encoding,
+            Unknown(_) => Self::UnknownSysError,
+        }
+    }
+}
---
+++ contracts/ics/conn/src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+mod entry;
+mod error;
+
+use ckb_std::default_alloc;
+
+ckb_std::entry!(program_entry);
+default_alloc!();
+
+fn program_entry() -> i8 {
+    match entry::main() {
+        Ok(_) => 0,
+        Err(err) => err as i8,
+    }
+}
